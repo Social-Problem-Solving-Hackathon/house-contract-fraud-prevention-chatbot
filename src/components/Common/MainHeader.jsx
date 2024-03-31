a75a84e2a8f3696e89f76f3c494f61cd82d78e04
nakyong82 <nakyong7770@gmail.com>
2024-04-01 00:32:05 +0900

    :sparkles: 알림창 데이터 추가

--- src/components/Common/MainHeader.jsx
+++ src/components/Common/MainHeader.jsx
@@ -6,97 +6,12 @@ import {
 import { Avatar, Badge, Popover } from "antd";
 import { useState } from "react";
 import styled from "styled-components";
+import Notifications from "./Notifications";
 
 const MainHeader = () => {
   const [show, setShow] = useState(true);
-  const [alertType, setAlertType] = useState(0);
 
-  const content = (
-    <AlertContainter>
-      <AlertTabContainer>
-        <AlertTabButton
-          onClick={() => setAlertType(0)}
-          className={alertType === 0 && "alertSelected"}
-        >
-          전체
-        </AlertTabButton>
-        <AlertTabButton
-          onClick={() => setAlertType(1)}
-          className={alertType === 1 && "alertSelected"}
-        >
-          결제
-        </AlertTabButton>
-        <AlertTabButton
-          onClick={() => setAlertType(2)}
-          className={alertType === 2 && "alertSelected"}
-        >
-          개정 법률
-        </AlertTabButton>
-      </AlertTabContainer>
-      <AlertContentBox>
-        <AlertContent>
-          <AlertTitle>
-            제목~~~~~<AlertDate>2024.01.01</AlertDate>
-          </AlertTitle>
-          <AlertText>
-            내용인델겨ㅙㅑ푸ㅐㅇ러퓨ㅐㅈ댜로갿휴ㅗ
-            ㅣㄴ우챈랴ㅠㄱ쟈ㅕ햊ㄷ루ㅐㅓㅠ
-            내용인델겨ㅙㅑ푸ㅐㅇ러퓨ㅐㅈ댜로갿휴ㅗ
-            ㅣㄴ우챈랴ㅠㄱ쟈ㅕ햊ㄷ루ㅐㅓㅠ
-            내용인델겨ㅙㅑ푸ㅐㅇ러퓨ㅐㅈ댜로갿휴ㅗ
-            ㅣㄴ우챈랴ㅠㄱ쟈ㅕ햊ㄷ루ㅐㅓㅠ
-            내용인델겨ㅙㅑ푸ㅐㅇ러퓨ㅐㅈ댜로갿휴ㅗ
-            ㅣㄴ우챈랴ㅠㄱ쟈ㅕ햊ㄷ루ㅐㅓㅠ
-          </AlertText>
-        </AlertContent>
-        <AlertContent>
-          <AlertTitle>
-            제목~~~~~<AlertDate>2024.01.01</AlertDate>
-          </AlertTitle>
-          <AlertText>
-            내용인델겨ㅙㅑ푸ㅐㅇ러퓨ㅐㅈ댜로갿휴ㅗ
-            ㅣㄴ우챈랴ㅠㄱ쟈ㅕ햊ㄷ루ㅐㅓㅠ
-          </AlertText>
-        </AlertContent>
-        <AlertContent>
-          <AlertTitle>
-            제목~~~~~<AlertDate>2024.01.01</AlertDate>
-          </AlertTitle>
-          <AlertText>
-            내용인델겨ㅙㅑ푸ㅐㅇ러퓨ㅐㅈ댜로갿휴ㅗ
-            ㅣㄴ우챈랴ㅠㄱ쟈ㅕ햊ㄷ루ㅐㅓㅠ
-          </AlertText>
-        </AlertContent>
-        <AlertContent>
-          <AlertTitle>
-            제목~~~~~<AlertDate>2024.01.01</AlertDate>
-          </AlertTitle>
-          <AlertText>
-            내용인델겨ㅙㅑ푸ㅐㅇ러퓨ㅐㅈ댜로갿휴ㅗ
-            ㅣㄴ우챈랴ㅠㄱ쟈ㅕ햊ㄷ루ㅐㅓㅠ
-          </AlertText>
-        </AlertContent>
-        <AlertContent>
-          <AlertTitle>
-            제목~~~~~<AlertDate>2024.01.01</AlertDate>
-          </AlertTitle>
-          <AlertText>
-            내용인델겨ㅙㅑ푸ㅐㅇ러퓨ㅐㅈ댜로갿휴ㅗ
-            ㅣㄴ우챈랴ㅠㄱ쟈ㅕ햊ㄷ루ㅐㅓㅠ
-          </AlertText>
-        </AlertContent>
-        <AlertContent>
-          <AlertTitle>
-            제목~~~~~<AlertDate>2024.01.01</AlertDate>
-          </AlertTitle>
-          <AlertText>
-            내용인델겨ㅙㅑ푸ㅐㅇ러퓨ㅐㅈ댜로갿휴ㅗ
-            ㅣㄴ우챈랴ㅠㄱ쟈ㅕ햊ㄷ루ㅐㅓㅠ
-          </AlertText>
-        </AlertContent>
-      </AlertContentBox>
-    </AlertContainter>
-  );
+  const content = <Notifications />;
 
   return (
     <Container>
@@ -113,7 +28,7 @@ const MainHeader = () => {
         <Popover
           placement="bottomRight"
           content={content}
-          title="알림창"
+          title="Notifications"
           style={{ verticalAlign: "center" }}
         >
           <Badge dot={show}>
@@ -163,68 +78,5 @@ const ButtonContainer = styled.div`
   gap: 1.5rem;
   //color: white;
 `;
-
-const AlertContainter = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding-right: 0.5rem;
-  width: 26rem;
-  max-height: 40dvw;
-  overflow-y: scroll;
-`;
-
-const AlertTabContainer = styled.div`
-  display: flex;
-  //justify-content: center;
-  align-items: center;
-  gap: 0.6rem;
-  margin-bottom: 0.5rem;
-  padding: 0.5rem 0;
-
-  .alertSelected {
-    color: white;
-    background-color: #2194f3;
-    border-color: white;
-  }
-`;
-
-const AlertTabButton = styled.button`
-  border: 1px solid black;
-  border-radius: 10rem;
-  background-color: white;
-`;
-
-const AlertContentBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-  padding: 0.3rem;
-  width: 100%;
-  border-radius: 0.5rem;
-  // background-color: #c6c6c6;
-`;
-
-const AlertContent = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 1rem;
-  border-radius: 0.5rem;
-  background-color: #f5f7fa;
-`;
-
-const AlertTitle = styled.div`
-  font-size: 1rem;
-  font-weight: 700;
-  color: black;
-`;
-
-const AlertDate = styled.span`
-  margin-left: 1rem;
-  font-size: 0.8rem;
-  font-weight: 400;
-  color: gray;
-`;
-
-const AlertText = styled.div``;
 
 export default MainHeader;
---
+++ src/components/Common/Notifications.jsx
@@ -0,0 +1,136 @@
+import { useState } from "react";
+import styled from "styled-components";
+import totalData from "../../assets/json/Notifications/notificationData.json";
+import paymentData from "../../assets/json/Notifications/paymentData.json";
+import revisedLawData from "../../assets/json/Notifications/revisedLawData.json";
+import communityData from "../../assets/json/Notifications/communityData.json";
+
+const Notifications = () => {
+  const [alertType, setAlertType] = useState(0);
+  const [alertData, setAlertData] = useState(totalData);
+
+  const onClickMenu = (menu) => {
+    setAlertType(menu);
+    setAlertData(selectedData(menu));
+  };
+
+  const selectedData = (menu) => {
+    if (menu === 0) {
+      return totalData;
+    } else if (menu === 1) {
+      return paymentData;
+    } else if (menu === 2) {
+      return revisedLawData;
+    } else if (menu === 3) {
+      return communityData;
+    }
+  };
+  return (
+    <AlertContainter>
+      <AlertTabContainer>
+        <AlertTabButton
+          onClick={() => onClickMenu(0)}
+          className={alertType === 0 && "alertSelected"}
+        >
+          Total
+        </AlertTabButton>
+        <AlertTabButton
+          onClick={() => onClickMenu(1)}
+          className={alertType === 1 && "alertSelected"}
+        >
+          Payments
+        </AlertTabButton>
+        <AlertTabButton
+          onClick={() => onClickMenu(2)}
+          className={alertType === 2 && "alertSelected"}
+        >
+          Revised Laws
+        </AlertTabButton>
+        <AlertTabButton
+          onClick={() => onClickMenu(3)}
+          className={alertType === 3 && "alertSelected"}
+        >
+          Community
+        </AlertTabButton>
+      </AlertTabContainer>
+      <AlertContentBox>
+        {alertData &&
+          alertData.map((item, index) => (
+            <AlertContent key={index}>
+              <AlertTitle>
+                {item.title}
+                <AlertDate>{item.data}</AlertDate>
+              </AlertTitle>
+              <AlertText>{item.content}</AlertText>
+            </AlertContent>
+          ))}
+      </AlertContentBox>
+    </AlertContainter>
+  );
+};
+
+const AlertContainter = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-right: 0.5rem;
+  width: 30rem;
+  max-height: 60dvh;
+  overflow-y: scroll;
+`;
+
+const AlertTabContainer = styled.div`
+  display: flex;
+  //justify-content: center;
+  align-items: center;
+  gap: 0.6rem;
+  margin-bottom: 0.5rem;
+  padding: 0.5rem 0;
+
+  .alertSelected {
+    color: white;
+    background-color: #2194f3;
+    border-color: white;
+  }
+`;
+
+const AlertTabButton = styled.button`
+  border: 1px solid black;
+  border-radius: 10rem;
+  background-color: white;
+`;
+
+const AlertContentBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  padding: 0.3rem;
+  width: 100%;
+  border-radius: 0.5rem;
+  // background-color: #c6c6c6;
+`;
+
+const AlertContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
+  border-radius: 0.5rem;
+  background-color: #f5f7fa;
+`;
+
+const AlertTitle = styled.div`
+  font-size: 1rem;
+  font-weight: 700;
+  color: black;
+`;
+
+const AlertDate = styled.span`
+  margin-left: 1rem;
+  font-size: 0.8rem;
+  line-height: 1rem;
+  font-weight: 400;
+  color: gray;
+`;
+
+const AlertText = styled.div``;
+
+export default Notifications;
